ae7397f0b69e34222fd0ef148395fd4d7eabf4f8
John F. Davis <davisjf@gmail.com>
2013-09-14 01:25:33 +0000

    Added the stack files.

---
+++ stack.c
@@ -0,0 +1,33 @@
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include "stack.h"
+#include "main.h"
+
+
+stack_t  STACK;
+
+
+void push(stack_t *ps, char *buffer) {
+
+    if (ps->size == STACKSIZE) {
+        fputs("Error: stack overflow\n", stderr);
+        abort();
+    } else {
+        //ps->items[ps->size++] = x;
+		strcpy(ps->items[ps->size++],buffer);
+	}
+
+}
+
+
+char * pop(stack_t *ps) {
+
+    if (ps->size == 0){
+        fputs("Error: stack underflow\n", stderr);
+        abort();
+    } else {
+        return ps->items[--ps->size];
+	}
+}
---
+++ stack.h
@@ -0,0 +1,17 @@
+// This is a stack that can store strings of 256 chars per stack element.
+// The stack itself can only hold 25 strings.  Since the stack in this
+// project holds a single definition and no definition is more than 15 
+// lines this is sufficient.
+
+#define STACKSIZE 25
+struct stack {
+    size_t size;
+    char items[STACKSIZE][256];
+};
+typedef struct stack stack_t;
+
+void push(stack_t *ps, char *buffer);
+
+char * pop(stack_t *ps);
+
+
